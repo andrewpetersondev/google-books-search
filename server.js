65a8a43b030fb2b77ed4f8b6833fbed7dedf572a
Andrew P <andrewpetersondev@gmail.com>
2019-09-13 18:08:35 -0500

    more updates for configuration

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+// npm packages
 const express = require("express");
+const mongoose = require("mongoose");
+
+// node packages
 const path = require("path");
+
+// other requirements for server
+// const apiRoutes = require("./routes/apiRoutes");
+const routes = require("./routes");
+
+// express configuration
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,14 +21,27 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// activity 7
+// Use apiRoutes
+// app.use("/api", apiRoutes);
 
-// Send every other request to the React app
+// activity 11 react router
+// add routes, both API and view
+app.use(routes);
+
+// activity 7
+// Send every request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
+// connect to mongo db
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+);
+
+// start the API server
+app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
