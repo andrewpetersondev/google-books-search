73a4907f33f13e9d0f85304c15d1b5b50d6fb593
Andrew P <andrewpetersondev@gmail.com>
2019-09-13 20:59:44 -0500

    added basic routes

---
+++ routes/api/books.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// matches with "/api/books"
+router
+  .route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
+
+module.exports = router;
