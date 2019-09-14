e6c702bd11ef11a35a5a334396af0364d96e3c48
Andrew P <andrewpetersondev@gmail.com>
2019-09-13 22:17:05 -0500

    added formatting to the homepage

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -78,6 +78,29 @@ class Books extends Component {
             </form>
           </Col>
         </Row>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>Search Results</h1>
+            </Jumbotron>
+            {this.state.books.length ? (
+              <List>
+                {this.state.books.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
       </Container>
     );
   }
